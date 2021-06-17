a50a6c9fe6123afc9e876258b460c0ee845976fd
Matthew <mattj20@gmail.com>
2021-06-16 18:44:27 -0700

    commit tree 1b8c4e6e74cfa27e0aece3b1f7b27c89d1c81206

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,65 @@
+def sort_array_asc(array)
+    array.sort do |a,b|
+        if a == b
+            0
+        elsif a < b 
+            -1
+        elsif a > b
+            1
+        end
+    end
+end
 
+def sort_array_desc(array)
+    array.sort do |a,b|
+        if a == b
+            0
+        elsif a > b 
+            -1
+        elsif a < b
+            1
+        end
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort{|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    return array
+end
+
+def reverse_array(array)
+    return array.reverse()
+end
+
+def kesha_maker(array)
+    kesha = []
+    array.each do |word|
+      word_array = word.split ""
+      word_array[2] = "$"
+      kesha << word_array.join
+    end
+    kesha
+end
+
+
+def find_a(array)
+    array.select{|string| string.start_with?("a")}
+  end
+  
+def sum_array(integers)
+    integers.inject{|sum, n| sum + n}
+end
+  
+def add_s(array)
+    array.each_with_index.collect do |string, index|
+     if index == 1
+     string
+     else
+     string << "s"
+     end
+    end
+end
